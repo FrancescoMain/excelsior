a141451d1992ba2f763c128ceb7de6828e768fe8
Francesco <cesaranofrancescomain@gmail.com>
2022-12-13 22:16:38 +0100

    aggiunto h1 di prova

--- src/components/LogApp.vue
+++ src/components/LogApp.vue
@@ -5,9 +5,16 @@
 </script>
 
 <template>
-
+  <div class="wrap">
+    <div class="jumbo"></div>
+    <div class="form">
+      <h1>Log In</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.wrap {
+  display: flex;
+}
 </style>
